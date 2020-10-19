a4f431bc943ae2ac1c0be2f0f44aa8beee4aac96
CHIHIRO KATO <chihiro@CHIHIROnoMacBook-Pro.local>
2020-10-19 05:12:25 -0700

    added js

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,5 @@
 var audio2 = new Audio();
-audio2.src = 'https://raw.githubusercontent.com/CHIHIROKATOca/quizofjapanese/master/audio/bgm.mp3';
+audio2.src = 'https://raw.githubusercontent.com/CHIHIROKATOca/quizofjapanese/master/audio/bgm2.mp3';
 
 window.onload = function(){
   audio2.play();
